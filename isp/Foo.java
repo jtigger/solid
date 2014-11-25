6f548885ce064e0d6fe589eb38e455370290f876
John S. Ryan <jtigger@infosysengr.com>
2014-11-25 06:43:29 -0800

    Renamed Java example.

--- isp/Foo.java
+++ isp/Foo.java
@@ -12,15 +12,15 @@ public interface MutableFoo extends Foo {
 }
 
 public class FooImpl implements Foo, MutableFoo {
-  public String getName();
-  public void setName(String name);
+  public String getName() { ... }
+  public void setName(String name) { ... }
 
-  public Bar getCollaborator();
-  public void setCollaborator(Bar collaborator);
+  public Bar getCollaborator() { ... }
+  public void setCollaborator(Bar collaborator) { ... }
 
-  public List<Datum> getData();
-  public void addDatum(Datum datum);
-  public void removeDatum(Datum datum);
+  public List<Datum> getData() { ... }
+  public void addDatum(Datum datum) { ... }
+  public void removeDatum(Datum datum) { ... }
 
   private String name;
   private Bar collaborator;
